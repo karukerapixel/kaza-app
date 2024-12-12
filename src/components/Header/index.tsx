137865aec200a7f91eee8d5095ba2f1b9a4a389b
karukerapixel <karukerapixel@gmail.com>
2024-12-12 19:33:10 +0100

    navbar deleted

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,12 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { TbWorld } from 'react-icons/tb';
-import { FiUsers } from 'react-icons/fi';
-import { HeaderLogo, HeaderNavBar, HeaderWrapper } from './style';
-import SearchBar from '../SearchBar';
+import { HeaderLogo, HeaderWrapper } from './style';
 import logo from '../../assets/logo.png';
 
 const Header: React.FC = () => {
   return (
     <HeaderWrapper>
       <HeaderLogo to={'/'}>
-        <img src={logo} alt="logo de l'entreprise Kaza" />
+        <img src={logo} alt="logo du site" />
       </HeaderLogo>
-      <SearchBar />
-      <HeaderNavBar>
-        <NavLink to={'/'}>Louer mon logement</NavLink>
-        <NavLink to={'/'} role={'lang'}>
-          <TbWorld />
-        </NavLink>
-        <NavLink to={'/'} role={'login'}>
-          <FiUsers />
-        </NavLink>
-      </HeaderNavBar>
     </HeaderWrapper>
   );
 };
