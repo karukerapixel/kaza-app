003c5eabaec297908882c2a1b0b1fd99d9ec977e
karukerapixel <karukerapixel@gmail.com>
2024-12-12 19:33:27 +0100

    add changes

--- src/components/Header/style.tsx
+++ src/components/Header/style.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export const HeaderWrapper = styled.header`
   z-index: 1000;
   width: 90%;
-  height: 120px;
+  height: 100px;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -20,21 +20,6 @@ export const HeaderLogo = styled(Link)`
 
   > img {
     width: 125px;
-  }
-`;
-
-export const HeaderNavBar = styled.nav`
-  display: flex;
-  align-items: center;
-  gap: 20px;
-
-  > a {
-    display: flex;
-    align-items: center;
-
-    > svg {
-      width: 25px;
-      height: 25px;
-    }
+    height: 41px;
   }
 `;
